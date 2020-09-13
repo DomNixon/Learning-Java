cc81a327010f34e757e43f70a2e94ba0dacdb754
Dominic Nixon <1761152+DomNixon@users.noreply.github.com>
2020-09-13 02:47:15 -0400

    Added CapitalSplit

---
+++ src/com/company/CapitalSplit.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class CapitalSplit
+{
+    public static String capSpace(String txt)
+    {
+    //capture data and split at UpperCase, inserting space at split and return all lower case.
+        //String txt = "iLoveMyTeapot";
+        String [] a = txt.split("(?=[A-Z])");
+        int i =0;
+        String answer = "";
+        while ( i < a.length )
+        {
+            answer = answer.concat(a[i] + " ");
+            System.out.println(i);
+            i++;
+        }
+        //Ensure all lower case and trim space
+        answer = answer.replaceAll("([A-Z])", "$1").toLowerCase();
+        answer = answer.trim();
+        System.out.println(answer);
+        return (answer);
+    }
+}
